1977471e7766102cddc7de43783fd7835adecc51
Kaung Shein <nicholasling1412@gmail.com>
2022-02-26 13:08:14 -0800

    Start PassGenerator and generatePassword

---
+++ generatePassword.js
@@ -0,0 +1,42 @@
+let leetSpeakList = {
+  a: ['4', '@', 'Д'],
+  d: ['[)', '|}', '|>', '>', 'cl', 'c1'],
+  k: ['|{', 'l<', '1<', '|<'],
+  m: ['[V]', '(V)', '1v1', ']V['],
+  n: ['И', '/V', 'ท'],
+  r: ['Я', '®'],
+};
+
+let generatePassword = (
+  isReverseDictOptimized,
+  isDictOptimized,
+  isLeeted,
+  isUppercased
+) => {
+  let password = 'aardvark';
+  if (isReverseDictOptimized && isDictOptimized) {
+    let midAlphabet = ['m', 'n'];
+    password = midAlphabet[Math.round(Math.random())] + password;
+  } else if (isDictOptimized) {
+    password = 'z' + password;
+  }
+  password = password
+    .split('')
+    .map((digit) => {
+      let replaceBank = [digit];
+      if (isUppercased) {
+        replaceBank.push(digit.toUpperCase());
+      }
+      if (isLeeted) {
+        replaceBank = replaceBank.concat(leetSpeakList[digit] || []);
+      }
+      return digit.replace(
+        digit,
+        replaceBank[Math.floor(Math.random() * replaceBank.length)]
+      );
+    })
+    .join('');
+  return password;
+};
+
+export default generatePassword;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
+import PassGenerator from './components/PassGenerator';
 
 function App() {
   return (
     <>
       <div className="text-3xl font-bold underline">Hello</div>
       <Link to="/about">About</Link>
+      <PassGenerator />
     </>
   );
 }
