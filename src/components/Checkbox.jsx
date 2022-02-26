ad3d8f6c579d7dcc8d2ca5fc360874f10cc74a87
Kaung Shein <nicholasling1412@gmail.com>
2022-02-26 14:52:37 -0800

    Fix logic for regular vs reverse

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,14 +1,23 @@
-export default function Checkbox({ value, name, desc, handleOnChange }) {
+export default function Checkbox({
+  value,
+  name,
+  desc,
+  long_desc,
+  handleOnChange,
+  disabled,
+}) {
   return (
-    <>
+    <div>
       <input
         type="checkbox"
         id={name}
         name={name}
         checked={value}
         onChange={() => handleOnChange(name)}
+        disabled={disabled}
       />
       <label htmlFor={name}>{desc}</label>
-    </>
+      <p>{long_desc}</p>
+    </div>
   );
 }
--- src/components/PassGenerator.jsx
+++ src/components/PassGenerator.jsx
@@ -8,11 +8,39 @@ export default function PassGenerator() {
     isDictOptimized: false,
     isUppercased: false,
     isLeeted: false,
+    isBothOptimized: false,
   });
   const [generatedPassword, setGeneratedPassword] = useState('');
 
   function handleOnChange(name) {
-    setOpts({ ...opts, [name]: !opts[name] });
+    if (name === 'isBothOptimized') {
+      if (opts.isBothOptimized) {
+        setOpts({
+          ...opts,
+          isBothOptimized: false,
+          isReverseDictOptimized: true,
+          isDictOptimized: false,
+        });
+      } else {
+        setOpts({
+          ...opts,
+          isBothOptimized: true,
+          isReverseDictOptimized: true,
+          isDictOptimized: true,
+        });
+      }
+    } else if (
+      name === 'isDictOptimized' ||
+      name === 'isReverseDictOptimized'
+    ) {
+      setOpts({
+        ...opts,
+        isReverseDictOptimized: !opts.isReverseDictOptimized,
+        isDictOptimized: !opts.isDictOptimized,
+      });
+    } else {
+      setOpts({ ...opts, [name]: !opts[name] });
+    }
   }
 
   function handleOnSubmit(evt) {
@@ -29,29 +57,42 @@ export default function PassGenerator() {
 
   return (
     <div>
-      <form onSubmit={(evt) => handleOnSubmit(evt)}>
+      <form onSubmit={(evt) => handleOnSubmit(evt)} className="flex flex-col">
         <Checkbox
           value={opts.isReverseDictOptimized}
           name="isReverseDictOptimized"
           desc="Optimize for Reverse Dictionary Attacks"
+          long_desc="By optimizing your password for reverse dictionary attacks, you allow other users to be hacked first. Outrunning the slowest of the group and whatnot."
           handleOnChange={handleOnChange}
+          disabled={opts.isBothOptimized}
         />
         <Checkbox
           value={opts.isDictOptimized}
           name="isDictOptimized"
-          desc="Optimize for the more familiar Dictionary Attacks"
+          desc="Optimize for Regular Dictionary Attacks"
+          long_desc="Sometimes it is better to simply brace for regular dictionary attacks. We barely understand what regular dictionary attacks are, why reverse it?"
+          handleOnChange={handleOnChange}
+          disabled={opts.isBothOptimized}
+        />
+        <Checkbox
+          value={opts.isBothOptimized}
+          name="isBothOptimized"
+          desc="Optimize for Both types of Dictionary Attacks"
+          long_desc="Sometimes it is better to simply brace for regular dictionary attacks. We barely understand what regular dictionary attacks are, why reverse it?"
           handleOnChange={handleOnChange}
         />
         <Checkbox
           value={opts.isUppercased}
           name="isUppercased"
-          desc="Include uppercase letters for better security while looking the same so you don't forget it"
+          desc="Include Uppercase Letters"
+          long_desc="Uppercase some letters for better security while looking the same so you don't forget it"
           handleOnChange={handleOnChange}
         />
         <Checkbox
           value={opts.isLeeted}
           name="isLeeted"
-          desc="Leet speak your password so AI robots cannot decipher it"
+          desc="Include Leeted Letters"
+          long_desc="Leet speak your password so AI robots have a harder time deciphering it"
           handleOnChange={handleOnChange}
         />
         <button type="submit">Submit</button>
