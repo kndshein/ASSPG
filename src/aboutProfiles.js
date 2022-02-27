59a54a53d102060f05c6c9305e883529fcf87808
Kaung Shein <nicholasling1412@gmail.com>
2022-02-27 03:25:50 -0800

    Copy for about page

---
+++ src/aboutProfiles.js
@@ -0,0 +1,42 @@
+let profiles = [
+  {
+    name: 'Paul',
+    position: 'CEO, Founder',
+    desc: 'Much like a Silicon Valley startup founder, Paul prides himself with coming up with ideas and having others do the work. ',
+  },
+  {
+    name: 'Kaung',
+    position: 'CTO',
+    desc: 'From years of experience as a CTO, Kaung has learned that by having unmaintainable code, he is saving the company tons of money by not having to put up security measures to guard against code theft.',
+  },
+  {
+    name: 'Paul',
+    position: 'Senior Systems Administrator',
+    desc: "Paul really knows how to computer a computer really really well. Though his favorite OS is Windows, he likes to tell others that he only uses 'Linus'. Paul thinks that Linus from 'Linus Tech Tips' refers to the Linux operating system.",
+  },
+  {
+    name: 'Kaung',
+    position: 'Senior Software Developer',
+    desc: "Kaung has a Bachelor's of Science in Art and a background in being a background actor. His software development career started when he was confused by how the 'Enhance Button' worked during one of his background appearances in X-Files",
+  },
+  {
+    name: 'Paul',
+    position: 'Tiktok Social Media Manager',
+    desc: "Paul has quadrupled ASSPG's Tiktok follower count from 12 to 36 since his hire, and he only had to buy 20 of them. The other four followers are his mother, his sister, his estranged 30-year old son, and his other ex-wife.",
+  },
+  {
+    name: 'Kaung',
+    position: 'Junior Developer',
+    desc: 'Ever since graduating from his software development bootcamp, Kaung has gathered 4 years of unpaid work experience. He has accepted this role in exchange for 800 exposure points and one time purchase of a burrito.',
+  },
+  {
+    name: 'Paul',
+    position: 'Intern',
+    desc: 'Paul leverages his four-year degree in Anthropology and Environmental Science with Departmental Honors to provide the team with the freshest coffee. He sure brings a lot to the table, sometimes even donuts.',
+  },
+  {
+    name: 'Kaung',
+    position: "Intern's Intern",
+    desc: 'Well, someone has to fetch coffee for the intern.',
+  },
+];
--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -2,8 +2,35 @@ import { Link } from 'react-router-dom';
 export default function AboutPage() {
   return (
     <>
-      <h1>About Page</h1>
-      <Link to="/">Home</Link>
+      <h4>Our Product</h4>
+      <p>
+        As an ASSPG user (ASSPG-er), you can be rest assured that you will
+        receive an industry-leading secured password produced by our AI's
+        artificial neural network hybrid-cloud blockchain engine. The generated
+        passwords have been tested vigorously -- from regression testing to
+        taste testing - we employ the use of cutting-edge technology wherever
+        possible. It has been statistically proven that compared to an average
+        user, ASSPG-ers' information are the least penetrated by most hacking
+        methods. Once you've been onboarded as an ASSPG-er, you will not only
+        receive a lifetime of backend support, but also receive access to our
+        Discord server. Take care of your private systems, become an ASSPG-er
+        today!
+      </p>
+      <h4>Our History</h4>
+      <p>
+        After having our cybersecurity firm being hacked into for the fifth time
+        in 3 months, we realized that using the string 'password' as our
+        database's password is no longer cutting it. So, we set out on a mission
+        to build a password generating service that we would be proud to call
+        someone else's. In fact, that's what we did -- we called somebody. We
+        reached out to our aforementioned hackers and inquired about their
+        process behind their work. By having their perspective, we were able to
+        understand the ins-and-outs of today's cybersecurity industry trends. We
+        then teamed up to provide a service, levering our synergy as hacker and
+        hackee -- they provide consultations on how to better secure our
+        systems, and we in turn provide them our users' data. It's a win-win!
+      </p>
+      <h4>Our Backgrounds</h4>
     </>
   );
 }
