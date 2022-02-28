b5781fdda45af1aaf829948cab06cffa9c6a6882
Kaung Shein <nicholasling1412@gmail.com>
2022-02-28 00:44:40 -0800

    Added darkmode for Checkbox

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -43,25 +43,43 @@ export default function Checkbox({
   return (
     <section className="flex p-2">
       <div
-        className={`flex shrink-0 justify-center items-center mr-4 mt-1.5 w-14 h-14 border-gray-500 border-4 rounded-lg transition ease-in-out ${colorConfig[name].borderHover}`}
-        style={{ borderColor: disabled && 'lightgrey' }}
+        className={`flex shrink-0 justify-center items-center mr-4 mt-1.5 w-14 h-14 border-4 rounded-lg dark:rounded-xl transition ease-in-out ${
+          disabled
+            ? 'border-gray-300 dark:border-gray-600'
+            : `border-gray-500 dark:border-gray-200 ${colorConfig[name].borderHover}`
+        }`}
       >
         <button
-          className={`h-10 w-10 p-3 m-1 rounded-md transition ease-in-out ${disabled ? 'bg-gray-300' : `${
-              value && `bg-gradient-to-r ${colorConfig[name].gradient}`} hover:shadow-md active:shadow-inner`}`}
+          className={`h-10 w-10 p-3 m-1 rounded-md transition ease-in-out ${
+            disabled
+              ? 'bg-gray-300 dark:bg-gray-600'
+              : `${
+                  value && `bg-gradient-to-r ${colorConfig[name].gradient}`
+                } hover:shadow-md active:shadow-inner`
+          }`}
           onClick={() => handleOnClick(name)}
           disabled={disabled}
         />
       </div>
       <section className="flex flex-col">
         <p
-          className={`font-bold text-lg ${disabled ? 'text-gray-300' : `${value ? `${colorConfig[name].textColor}` : 'text-gray-500'}`}`}
+          className={`font-bold text-lg ${
+            disabled
+              ? 'text-gray-300 dark:text-gray-600'
+              : `${
+                  value
+                    ? `${colorConfig[name].textColor}`
+                    : 'text-gray-500 dark:text-gray-200'
+                }`
+          }`}
         >
           {desc}
         </p>
-        <p className="text-gray-400">{long_desc}</p>
+        <p className="text-gray-400 dark:text-gray-500">{long_desc}</p>
         {example && (
-          <p className="text-gray-500 italic mt-0.5">Examples: {example}</p>
+          <p className="text-gray-500 italic mt-0.5 dark:text-gray-600">
+            Examples: {example}
+          </p>
         )}
       </section>
     </section>
