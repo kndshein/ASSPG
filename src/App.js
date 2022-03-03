01408bc98035a630f43631c04bc5eff26f7745c2
Kaung Shein <nicholasling1412@gmail.com>
2022-03-02 22:12:48 -0800

    Replace light theme in App.js

--- src/App.js
+++ src/App.js
@@ -6,28 +6,26 @@ import AboutPage from './pages/AboutPage';
 function App() {
   return (
     <>
-      <nav className="flex justify-center items-center gap-8 p-4 mb-6 dark:bg-zinc-900">
+      <nav className="flex justify-center items-center gap-8 p-4 mb-6 bg-zinc-900">
         <NavLink
           className={({ isActive }) => {
             return `uppercase ${
               isActive
                 ? 'text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500'
-                : 'text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-200'
+                : 'text-gray-400 hover:text-gray-200'
             }`;
           }}
           to="/"
         >
           Engine
         </NavLink>
-        <h1 className="text-3xl font-bold text-center dark:text-white">
-          ASSPG
-        </h1>
+        <h1 className="text-3xl text-white font-bold text-center">ASSPG</h1>
         <NavLink
           className={({ isActive }) => {
             return `uppercase ${
               isActive
                 ? 'text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500'
-                : 'text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-200'
+                : 'text-gray-400 hover:text-gray-200'
             }`;
           }}
           to="/about"
