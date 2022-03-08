404f21bc9de13fe73bf8bc48443b6e2b7fa088ac
kndshein <kndshein@gmail.com>
2022-03-07 16:59:42 -0800

    Minor copy edits

--- src/components/PassGenerator.jsx
+++ src/components/PassGenerator.jsx
@@ -58,7 +58,7 @@ export default function PassGenerator() {
 
   useEffect(() => {
     loading &&
-      setTimeout(() => setLoading(false), (Math.random() + 0.5) * 1500);
+      setTimeout(() => setLoading(false), (Math.random() + 0.5) * 500);
   }, [loading]);
 
   function handleOnClick(name) {
@@ -160,7 +160,7 @@ export default function PassGenerator() {
           name="isPowered"
           desc="Double the Processing Power"
           price="¥200"
-          long_desc="Research shows that loading icons that spin for just the right amount of time make users think the server worked hard, r('murica) = .42, p = .69. (Saysnoone at el., 420 BC) This feature definitely doesn't just render a fake loading icon. How dare you accuse us?"
+          long_desc="Research shows that loading icons that spin for just the right amount of time make users think the server worked hard, r('murica) = .42, p = .69. (Saysnoone at el., 420 BC) Anyway, this feature totally does more than render a fake loading icon, why do you ask?"
           handleOnClick={handleOnClick}
         />
       </div>
